296a21a0dfc21f501e3f2646ca3856b1ff1e416d
marti19686 <marti19686@gmail.com>
2020-05-24 21:03:48 +0200

    welcome page edit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,93 +8,49 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Bitter&display=swap" rel="stylesheet">
 
         <!-- Styles -->
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
+            body
+            {
                 margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
+                padding: 0;
                 display: flex;
                 justify-content: center;
+                align-items: center;
+                height: 100vh;
+                background: #000000;
             }
-
-            .position-ref {
+            p
+            {
                 position: relative;
-            }
+                font-family: 'Bitter', serif;
+                font-size: 4em;
+                letter-spacing: 4px;
+                overflow: hidden;
+                background: linear-gradient(90deg, #000000, #ffffff, #000000);
+                background-size: 80%;
+                background-repeat: no-repeat;
+                animation: animate 3s linear infinite;
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: rgba(255,255,255,0);
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
             }
+            @keyframes animate {
+                0%
+                {
+                    background-position: -500%;
+                }
+                100%
+                {
+                    background-position: 500%;
+                }
 
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
             }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
+        <p>Waiting For the Unknown</p>
     </body>
 </html>
