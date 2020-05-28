fef84157d122b1386e749eb48cf806c42d04a235
marti19686 <marti19686@gmail.com>
2020-05-28 21:46:34 +0200

    portfolio v.1

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,26 @@
 require('./bootstrap');
+
+// $(document).ready(function (e) {
+//     $win = $(window);
+//     $navbar=$('#header');
+//     $toggle = $('.toggle-button');
+//     var width = $navbar.width();
+//     toggle_onclick($win, $navbar, width);
+//
+//     $win.resize(function(){
+//         toggle_onclick($win, $navbar, width);
+//     });
+//
+//     $toggle.click(function (e) {
+//         $navbar.toggleClass("toggle-left");
+//     })
+//
+// });
+//
+// function toggle_onclick($win, $navbar, width) {
+//     if($win.width() <= 768){
+//         $navbar.css({left:`-$(width)px`});
+//     } else {
+//         $navbar.css({left:'0px'});
+//     }
+// }
---
+++ resources/views/portfolio/header.blade.php
@@ -0,0 +1,27 @@
+<header id="header"r>
+    <div class="row m-0">
+        <div class="col-3 bgcolor-black">
+            <nav class="primary-nav navbar-expand-md">
+                <div class="site-title text-center text-light py-5">
+                    <a href="#" class="navbar-brand font-staat font-size-40">One-Moment</a>
+                    <p class="description text-uppercase font-os">Marcin Michalak</p>
+                </div>
+                <div class="d-flex flex-column">
+                    <a href="#" class="nav-item nav-link text-white-50 font-os font-size-16 active">Home</a>
+                    <a href="#" class="nav-item nav-link text-white-50 font-os font-size-16 active">About</a>
+                    <a href="#" class="nav-item nav-link text-white-50 font-os font-size-16 active">Services</a>
+                    <a href="#" class="nav-item nav-link text-white-50 font-os font-size-16 active">Portfolio</a>
+                    <a href="#" class="nav-item nav-link text-white-50 font-os font-size-16 active">News</a>
+                    <a href="#" class="nav-item nav-link text-white-50 font-os font-size-16 active">Contact us</a>
+                </div>
+            </nav>
+        </div>
+    </div>
+{{--    <button class="toggle-button"><span class="fas fa-bars fa-2x"></span></button>--}}
+    <div class="social">
+        <span class="mr-3"><i class="fab fa-facebook-f"></i> </span>
+        <span class="mr-3"><i class="fab fa-twitter"></i> </span>
+        <span class="mr-3"><i class="fab fa-instagram"></i> </span>
+        <span class="mr-3"><i class="fab fa-youtube"></i> </span>
+    </div>
+</header>
